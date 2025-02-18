a2d0e93bb88035e8675ae16960ef19c9467ca93c
allAboutJS <victoronah1914@gmail.com>
2025-02-18 19:30:46 +0100

    refactor: improve footer layout and responsiveness for better user experience

--- src/layouts/Public/components/Footer.tsx
+++ src/layouts/Public/components/Footer.tsx
@@ -7,60 +7,62 @@ const Footer = () => {
         <footer className="bg-gradient-to-b from-base to-[#2C418F] text-gray-200 py-12 pt-36 relative overflow-hidden">
             <div className="absolute h-[119px] w-[90%] rounded-t-full max-w-[1100px] bg-white top-[calc(100%-.35rem)] left-1/2 -translate-x-1/2"></div>
             <div className="container mx-auto px-6 text-[20px] font-light">
-                <div className="grid grid-cols-1 md:grid-cols-12 gap-10 px-16">
-                    <div className="md:col-span-4 flex flex-col items-center md:items-start">
+                <div className="flex max-lg:flex-col gap-10 px-16">
+                    <div className="flex flex-col items-center lg:items-start">
                         <img src={logo} alt="Hovertask Logo" className="w-36 mb-4" />
-                        <p className="max-w-xs text-center md:text-left">
+                        <p className="max-w-xs text-center lg:text-left">
                             Our mission is to create opportunities for people to earn daily income while helping
                             businesses reach a wider audience.
                         </p>
                     </div>
-                    <div className="md:col-span-3 text-center md:text-left">
-                        <h3 className="text-lg font-medium text-white mb-4">Quick Links</h3>
-                        <ul className="space-y-2">
-                            {["Home", "Marketplace", "About Us", "FAQ", "Contact Us"].map((link) => (
-                                <li key={link}>
-                                    <a href="#" className="hover:text-white hover:opacity-80 transition">
-                                        {link}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="md:col-span-2 text-center md:text-left">
-                        <h3 className="text-lg font-medium text-white mb-4">Useful Links</h3>
-                        <ul className="space-y-2">
-                            {["Privacy Policy", "Terms & Conditions", "Support"].map((link) => (
-                                <li key={link}>
-                                    <a href="#" className="hover:text-white hover:opacity-80 transition">
-                                        {link}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="md:col-span-3 flex flex-col items-center md:items-start">
-                        <h3 className="text-lg font-medium text-white mb-4">Install Our App</h3>
-                        <div className="flex-col flex gap-4">
-                            <a href="#">
-                                <img
-                                    src="/assets/images/Apple.png"
-                                    alt="App Store"
-                                    className="h-10 hover:opacity-80 transition object-contain"
-                                />
-                            </a>
-                            <a href="#">
-                                <img
-                                    src="/assets/images/Google.png"
-                                    alt="Google Play"
-                                    className="h-10 hover:opacity-80 transition object-contain"
-                                />
-                            </a>
+                    <div className="grid grid-cols-2 lg:grid-cols-3 flex-1 gap-10">
+                        <div>
+                            <h3 className="text-lg font-medium text-white mb-4">Quick Links</h3>
+                            <ul className="space-y-2">
+                                {["Home", "Marketplace", "About Us", "FAQ", "Contact Us"].map((link) => (
+                                    <li key={link}>
+                                        <a href="#" className="hover:text-white hover:opacity-80 transition">
+                                            {link}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 className="text-lg font-medium text-white mb-4">Useful Links</h3>
+                            <ul className="space-y-2">
+                                {["Privacy Policy", "Terms & Conditions", "Support"].map((link) => (
+                                    <li key={link}>
+                                        <a href="#" className="hover:text-white hover:opacity-80 transition">
+                                            {link}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="flex flex-col">
+                            <h3 className="text-lg font-medium text-white mb-4">Install Our App</h3>
+                            <div className="flex-col flex gap-4">
+                                <a href="#">
+                                    <img
+                                        src="/assets/images/Apple.png"
+                                        alt="App Store"
+                                        className="h-10 hover:opacity-80 transition object-contain"
+                                    />
+                                </a>
+                                <a href="#">
+                                    <img
+                                        src="/assets/images/Google.png"
+                                        alt="Google Play"
+                                        className="h-10 hover:opacity-80 transition object-contain"
+                                    />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="mt-12 pt-6">
-                    <div className="flex flex-col md:flex-row justify-between items-center gap-4 px-12">
+                    <div className="flex flex-col lg:flex-row justify-between items-center gap-4 px-12">
                         <p className="text-sm">
                             &copy; {new Date().getFullYear()} Hovertask. All rights reserved. Made by Aloyande Nurudeen
                         </p>
