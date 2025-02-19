a4c166fd3d706765b58cc5751059f74be2931562
allAboutJS <victoronah1914@gmail.com>
2025-02-19 02:33:26 +0100

    refactor: update about page layout and styling for improved readability and structure

--- src/pages/about/components/CoreValues.tsx
+++ src/pages/about/components/CoreValues.tsx
@@ -2,8 +2,8 @@ import { LuDot } from "react-icons/lu";
 
 const CoreValues = () => {
     return (
-        <div className="w-full flex flex-col bg-white items-center justify-center py-16 px-14 max-md:px-1">
-            <div className="w-full flex items-center justify-center  gap-5 mt-10 max-md:flex-col">
+        <div className="w-full flex flex-col bg-white items-center justify-center py-12 px-14 max-md:px-1">
+            <div className="w-full flex items-center justify-center gap-5 max-md:flex-col">
                 <div className="w-[40%] max-md:w-[90%]">
                     <div className="w-[60%] max-md:w-full max-md:justify-center bg-gradient-to-l from-[#DAE2FF]/10 to-[#DAE2FF] h-[7rem] flex justify-center items-center rounded-3xl transform rotate-[-2deg] ">
                         <p className="text-4xl font-light bg-gradient-to-l from-[#2C418F] to-base text-transparent bg-clip-text">
@@ -12,7 +12,7 @@ const CoreValues = () => {
                     </div>
                     <img src="/assets/images/Rectangle 39338.png" alt="Core Values" className="rounded-xl" />
                 </div>
-                <div className="w-[50%] flex flex-col gap-4 max-md:w-full">
+                <div className="w-[50%] flex flex-col gap-4 max-md:w-full space-y-3 leading-1.5">
                     <p className="text-lg flex items-center gap-2  max-md:text-center">
                         <LuDot className="text-base" size={22} />
                         <span className="font-bold text-base ">Transparency:</span> We ensure honesty and clarity in all
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,17 +1,15 @@
 import { RiGogglesLine, RiTeamLine } from "react-icons/ri";
-import SectionHeader from "./components/SectionHeader";
-import GradientBox from "./components/GradientBox";
 import CoreValues from "./components/CoreValues";
 
 const About = () => {
     return (
-        <div className="hero w-full h-auto">
-            <div className="w-full h-[20vh] flex justify-center gap-2 items-center flex-col">
-                <SectionHeader title="About Us - HoverTask" />
-                <p className="font-light text-xl">Introduction</p>
-            </div>
-            <div className="w-full h-[50vh] flex justify-center items-center max-md:h-[70vh]">
-                <div className="w-[80%] h-[60%] max-md:h-[90%] bg-[#e9edfe] flex justify-center items-center text-center rounded-4xl px-8 transform rotate-[-2deg]">
+        <div>
+            <section className="hero">
+                <div className="space-y-6 p-4 text-center py-16">
+                    <h1 className="text-[55.62px] mb-4 gradient-text">About Us - Hovertask</h1>
+                    <p className="text-[33.37px]">Introduction</p>
+                </div>
+                <div className="w-full max-w-[1223.5px] max-[1255px]:p-4 mx-auto bg-[#e9edfe] rounded-[44.5px] px-8 transform rotate-[-2deg] p-12 mb-36">
                     <p className="text-xl font-light max-md:text-lg">
                         Welcome to Hovertask, your trusted platform for earning and advertising! At Hovertask, we
                         connect businesses with individuals who are ready to perform simple social media tasks,
@@ -19,55 +17,46 @@ const About = () => {
                         earn daily income while helping businesses reach a wider audience.
                     </p>
                 </div>
-            </div>
-            <div className="w-full h-[70vh] flex justify-center gap-14 max-md:gap-0 items-center max-md:flex-col max-md:h-[90vh]">
-                <div className="w-[30%] h-[60%] flex justify-center items-center max-md:w-[80%]">
+                <div className="w-full flex justify-center gap-14 max-md:gap-0 items-center max-w-[1223.5px] mx-auto mb-36">
                     <img
                         src="/assets/images/Young african man having video call on laptop at home 1.png"
                         alt="Video Call"
-                        className="w-full h-auto"
+                        className="w-[40%] h-auto"
                         loading="lazy"
                     />
-                </div>
-                <div className="w-[32%] h-[47%] flex flex-col justify-center gap-3 px-3 max-md:w-[80%] max-md:h-[60%]">
-                    <p className="bg-gradient-to-r from-base to-[#2C418F] text-transparent bg-clip-text text-7xl font-light max-md:text-6xl">
-                        Our Mission
-                    </p>
-                    <p className="font-light">
-                        To empower individuals by providing them with easy, flexible ways to earn and to support
-                        businesses in achieving their marketing goals through innovative solutions.
-                    </p>
-                </div>
-            </div>
-            <div className="w-full flex justify-around flex-col items-center h-[70vh] px-12 max-md:h-[90vh] max-md:px-0">
-                <div className="w-full h-[40%] px-3">
-                    <div className="w-[60%] py-2 flex justify-start items-center max-md:w-full max-md:justify-center">
-                        <SectionHeader title="Our Vision" />
-                    </div>
-                    <GradientBox>
-                        <RiGogglesLine size={100} className="text-base max-md:size-[150px]" />
-                        <p className="text-light">
-                            To become the leading platform for micro-jobs, advertising, and product reselling in the
-                            digital space, empowering millions of users globally.
+                    <div className="flex flex-col justify-center gap-3 px-3">
+                        <h3 className="gradient-text text-6xl">Our Mission</h3>
+                        <p className="font-light text-[20px]">
+                            To empower individuals by providing them with easy, flexible ways to earn and to support
+                            businesses in achieving their marketing goals through innovative solutions.
                         </p>
-                    </GradientBox>
-                </div>
-                <div className="w-full h-[40%] flex justify-start items-end flex-col px-2">
-                    <div className="w-[60%] py-2 flex justify-start items-center max-md:w-full max-md:justify-center">
-                        <SectionHeader title="Our Team" />
                     </div>
-                    <GradientBox>
-                        <RiTeamLine size={100} className="text-base max-md:size-[150px]" />
-                        <p className="text-light">
-                            Hovertask is powered by a team of dedicated professionals who are passionate about creating
-                            opportunities for individuals and businesses.
-                        </p>
-                    </GradientBox>
                 </div>
-            </div>
-            <div>
-                <CoreValues />
-            </div>
+                <div className="w-full mb-18 space-y-18  max-w-[1223px] mx-auto max-[1255px]:p-4">
+                    <div className="max-w-[887.1px] w-full">
+                        <h3 className="gradient-text text-[41.26px]">Our Vision</h3>
+                        <div className="shadow-lg p-6 shadow-gray-200 bg-[#EEF0FF] rounded-3xl flex justify-center gap-14 items-center max-m px-2 text-2xl font-light">
+                            <RiGogglesLine size={100} className="text-base max-md:size-[150px]" />
+                            <p className="text-light">
+                                To become the leading platform for micro-jobs, advertising, and product reselling in the
+                                digital space, empowering millions of users globally.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="max-w-[887.1px] w-full float-right">
+                        <h3 className="gradient-text text-[41.26px]">Our Team</h3>
+                        <div className="shadow-lg p-6 shadow-gray-200 bg-[#00B3060D] rounded-3xl flex justify-center gap-14 items-center max-md:w-full px-2 text-2xl font-light">
+                            <RiTeamLine size={100} className="text-base max-md:size-[150px]" />
+                            <p className="text-light">
+                                Hovertask is powered by a team of dedicated professionals who are passionate about
+                                creating opportunities for individuals and businesses.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="clear-right"></div>
+                </div>
+            </section>
+            <CoreValues />
         </div>
     );
 };
