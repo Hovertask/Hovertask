d6f4e215240a607a47108f1b7e85cebb6f4d07bb
allAboutJS <victoronah1914@gmail.com>
2025-02-25 09:32:00 +0100

    refactor: enhance BannersCarousel with automatic slide transitions and improved indicator styling

--- src/pages/marketplace/components/Market.tsx
+++ src/pages/marketplace/components/Market.tsx
@@ -5,6 +5,7 @@ import { BiStar } from "react-icons/bi";
 import { FaHeart } from "react-icons/fa";
 import storeIcon from "../../../assets/store-button.svg";
 import arrow from "../../../assets/arrow.svg";
+import { useEffect, useRef, useState } from "react";
 
 interface Product {
     name: string;
@@ -165,18 +166,43 @@ const ProductCard = (props: Product) => {
 };
 
 const BannersCarousel = () => {
+    const carouselRef = useRef<HTMLDivElement>(null);
+    const [currentSlide, setCurrentSlide] = useState(1);
+
+    useEffect(() => {
+        const width = carouselRef.current?.clientWidth;
+        carouselRef.current?.scroll({ left: width! * (currentSlide - 1) });
+    }, [currentSlide, carouselRef.current]);
+
+    useEffect(() => {
+        const interval = setInterval(() => setCurrentSlide((prev) => (prev === 3 ? 1 : prev + 1)), 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="space-y-2">
-            <div className="flex overflow-auto no-scrollbar items-center">
+            <div ref={carouselRef} className="flex overflow-auto no-scrollbar items-center">
                 <img className="w-full h-auto" src="/assets/images/Group 1000004390.png" alt="Banner" />
                 <img className="w-full h-auto" src="/assets/images/Group 1000004393.png" alt="Banner" />
                 <img className="w-full h-auto" src="/assets/images/Group 1000004395.png" alt="Banner" />
             </div>
-            <div className="grid grid-cols-5 gap-1 w-[108px] mx-auto">
-                <span className="h-[3.47px] w-full col-span-2 bg-base"></span>
-                <span className="h-[3.47px] bg-[#B3B3B3] w-full"></span>
-                <span className="h-[3.47px] bg-[#B3B3B3] w-full"></span>
-                <span className="h-[3.47px] bg-[#B3B3B3] w-full"></span>
+            <div className="grid grid-cols-4 gap-1 w-[108px] mx-auto">
+                <span
+                    className={`${
+                        currentSlide === 1 ? "col-span-2 bg-base" : "bg-[#B3B3B3]"
+                    } h-[3.47px] w-full transition`}
+                ></span>
+                <span
+                    className={`${
+                        currentSlide === 2 ? "col-span-2 bg-base" : "bg-[#B3B3B3]"
+                    }  h-[3.47px] w-full transition"`}
+                ></span>
+                <span
+                    className={`${
+                        currentSlide === 3 ? "col-span-2 bg-base" : "bg-[#B3B3B3]"
+                    }  h-[3.47px] w-full transition"`}
+                ></span>
             </div>
         </div>
     );
