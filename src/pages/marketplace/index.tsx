1cd7b61d4f3884a217b796380da457eb28a55bf7
allAboutJS <victoronah1914@gmail.com>
2025-02-25 11:21:42 +0100

    refactor: adjust padding in Marketplace layout for improved responsiveness

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -4,7 +4,7 @@ import Market from "./components/Market";
 
 const Marketplace = () => {
     return (
-        <div className="hero overflow-hidden lg:p-4">
+        <div className="hero overflow-hidden lg:px-4">
             <div className="grid max-lg:grid-cols-1 gap-4 max-xl:grid-cols-[1fr_232px] xl:grid-cols-[243px_1fr_232px] max-w-screen-xl mx-auto">
                 <SideNav />
                 <Market />
