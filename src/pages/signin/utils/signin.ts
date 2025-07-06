bcd80be4dd99e7b1166a19504b97cccf545c354d
allAboutJS <victoronah1914@gmail.com>
2025-07-06 18:57:17 +0100

    CHORE: Update login api endpoint url

--- src/pages/signin/utils/signin.ts
+++ src/pages/signin/utils/signin.ts
@@ -2,7 +2,7 @@ import type { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 
 const signin = async (form: FieldValues) => {
-    const API_ENDPOINT = "https://backend.hovertask.com/api/v1/login";
+    const API_ENDPOINT = "https://backend.hovertask.com/api/login";
 
     try {
         const response = await fetch(API_ENDPOINT, {
@@ -16,9 +16,11 @@ const signin = async (form: FieldValues) => {
             window
                 .location
                 .assign(`https://hovertask-dashboard.vercel.app/?token=${data.token}`);
-        else  toast.error(data.message || "An error occurred. Please try again");
-    } catch (error: any) {
-        toast.error(error.message);
+        else toast.error(data.message || "An error occurred. Please try again");
+    } catch (error: unknown) {
+        if (error instanceof Error)
+            toast.error(error.message);
+        else toast.error("An unknown error ocurred")
     }
 };
 
